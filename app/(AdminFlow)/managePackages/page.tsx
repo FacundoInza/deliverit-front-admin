0de08386ed6954cab45bf293a2fb786a2c96ebb5
Anibal-Quiroz-P5 <anibal.quiroz.p5@gmail.com>
2023-08-07 08:44:15 -0300

    feat: package manage card

---
+++ app/(AdminFlow)/managePackages/page.tsx
@@ -0,0 +1,89 @@
+import React, { FC } from 'react';
+import { GeneralCard } from '../../../components/ui/generalCard/GeneralCard';
+import { DeliveryCard } from '../../../components/ui/deliveryCard/DeliveryCard';
+import { AiFillPlusCircle } from 'react-icons/ai';
+
+const InitWorkDay: FC = () => {
+    const dummyData = [
+        {
+            deliveryID: '#02A35',
+            deliveryAddress: 'Castillo 1356, CABA',
+            status: 'pending',
+        },
+        {
+            deliveryID: '#0H167',
+            deliveryAddress: 'Av. Carabobo y Rivadavia, CABA',
+            status: 'in progress',
+        },
+        {
+            deliveryID: '#0H166',
+            deliveryAddress: 'Mendoza 1810, CABA',
+            status: 'inactive',
+        },
+        {
+            deliveryID: '#0B540',
+            deliveryAddress: 'Scalabrini Ortiz 5073, CABA',
+            status: 'delivered',
+        },
+        {
+            deliveryID: '#0V768',
+            deliveryAddress: 'Pensamientos 2673, CABA',
+            status: 'pending',
+        },
+        /* {
+            deliveryID: '#0Z395',
+            deliveryAddress: 'Av. Acoyte 2907, CABA',
+            status: 'in progress',
+        },
+                {
+            deliveryID: '#0D832',
+            deliveryAddress: 'Libertador 1302, CABA',
+            status: 'inactive',
+        },
+                {
+            deliveryID: '#0N510',
+            deliveryAddress: 'Lavalleja 2174, CABA',
+            status: 'delivered',
+        }, */
+    ];
+
+    return (
+        <div
+            className=' rounded-xl relative'
+            style={{
+                height: '750px',
+                overflowY: 'scroll',
+                borderRadius: '0 0 20px 20px',
+            }}
+        >
+            <GeneralCard title='Packages'>
+                <div>
+                    <p className='text-primary font-bold mb-3.5'>
+                        {' '}
+                        523 paquetes
+                    </p>
+                </div>
+
+                {dummyData
+                    .filter((delivery) => delivery.status !== 'delivered')
+                    .map((delivery) => (
+                        <DeliveryCard
+                            key={delivery.deliveryID}
+                            deliveryID={delivery.deliveryID}
+                            deliveryAddress={delivery.deliveryAddress}
+                            status={delivery.status}
+                            showCancel={true}
+                        />
+                    ))}
+                <div className='flex'>
+                    <div className='ml-auto  px-1 py-5 '>
+                        <AiFillPlusCircle size={50} color='#80ED99' />
+                    </div>
+                </div>
+                {/* </div> */}
+            </GeneralCard>
+        </div>
+    );
+};
+
+export default InitWorkDay;
